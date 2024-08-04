aca1582f39438b49516431d9dd926bf8b7ab149d
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-08-04 05:16:28 -0700

    pythone

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Solves the lock boxes puzzle """
 
 
 def look_next_opened_box(opened_boxes):
+    """Looks for the next opened box
+    Args:
+        opened_boxes (dict): Dictionary which contains boxes already opened
+    Returns:
+        list: List with the keys contained in the opened box
+    """
     for index, box in opened_boxes.items():
         if box.get('status') == 'opened':
             box['status'] = 'opened/checked'
@@ -10,7 +17,12 @@ def look_next_opened_box(opened_boxes):
 
 
 def canUnlockAll(boxes):
-    
+    """Check if all boxes can be opened
+    Args:
+        boxes (list): List which contain all the boxes with the keys
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
     if len(boxes) <= 1 or boxes == [[]]:
         return True
 
