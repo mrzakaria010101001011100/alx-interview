8f4650aeaf4d2e81e3e74d3f1fc5c60806b86065
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-10-03 16:41:08 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,55 +1,79 @@
 #!/usr/bin/python3
+"""Module for Prime Game"""
 
-def sieve_of_eratosthenes(n):
-    """Returns a list of primes up to n using Sieve of Eratosthenes"""
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if sieve[i]:
-            for j in range(i*i, n + 1, i):
-                sieve[j] = False
-    return [i for i in range(2, n + 1) if sieve[i]]
 
 def isWinner(x, nums):
-    """Determines the winner of the prime game"""
-    if x < 1 or not nums:
-        return None
+    """
+    Determines the winner of a set of prime number removal games.
 
-    # Get the maximum number in nums for prime precomputation
-    max_n = max(nums)
-    
-    # Precompute primes up to the largest number in nums
-    primes_up_to_max = sieve_of_eratosthenes(max_n)
-    
-    # A list to keep track of how many primes are removable for each n
-    prime_count = [0] * (max_n + 1)
-    
-    # Fill prime_count array with the number of primes up to each number n
-    for i in range(2, max_n + 1):
-        prime_count[i] = prime_count[i - 1]
-        if i in primes_up_to_max:
-            prime_count[i] += 1
-    
-    # Initialize Maria and Ben's win count
-    maria_wins = 0
-    ben_wins = 0
-    
-    # For each round, determine the winner
-    for n in nums:
-        # Maria wins if the number of primes up to n is odd, else Ben wins
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
+
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
+
+    Raises:
+        None.
+    """
+    # Check for invalid input
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    # Initialize scores and array of possible prime numbers
+    ben = 0
+    maria = 0
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
+    a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    # Play each round of the game
+    for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            ben_wins += 1
-    
-    # Determine who won the most rounds
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+            maria += 1
+    # Determine the winner of the game
+    if ben > maria:
         return "Ben"
-    else:
-        return None
+    if maria > ben:
+        return "Maria"
+    return None
 
-# Example usage:
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+
+def rm_multiples(ls, x):
+    """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
+
+    Args:
+        ls (list of int): An array of possible prime numbers.
+        x (int): The prime number to remove multiples of.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
+    # This loop iterates over multiples of a prime number and marks them as
+    # non-prime by setting their corresponding value to 0 in the input
+    # list ls. Starting from 2, it sets every multiple of x up to the
+    # length of ls to 0. If the index i * x is out of range for the list ls,
+    # the try block will raise an IndexError exception, and the loop will
+    # terminate using the break statement.
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
